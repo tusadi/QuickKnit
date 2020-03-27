7e1dee6f6bb160c40a97b118c5bc0bb77278095e
Sireesha Putcha <sireesha@seas.upenn.edu>
2020-03-27 17:14:42 -0400

    Added plugin base code

---
+++ QuickKnit/QuickKnit/MayaIncludes.h
@@ -0,0 +1,40 @@
+#pragma once
+
+//All Maya include files and Macros 
+
+#include <maya/MPxCommand.h>
+#include <maya/MFnPlugin.h>
+#include <maya/MIOStream.h>
+#include <maya/MString.h>
+#include <maya/MArgList.h>
+#include <maya/MGlobal.h>
+#include <maya/MSimple.h>
+#include <maya/MDoubleArray.h>
+#include <maya/MPoint.h>
+#include <maya/MPointArray.h>
+#include <maya/MFnNurbsCurve.h>
+#include <maya/MDGModifier.h>
+#include <maya/MPlugArray.h>
+#include <maya/MVector.h>
+#include <maya/MFnDependencyNode.h>
+#include <maya/MStringArray.h>
+#include <maya/MGlobal.h>
+#include <maya/MTime.h>
+#include <maya/MFnMesh.h>
+#include <maya/MPoint.h>
+#include <maya/MFloatPoint.h>
+#include <maya/MFloatPointArray.h>
+#include <maya/MIntArray.h>
+#include <maya/MDoubleArray.h>
+#include <maya/MFnUnitAttribute.h>
+#include <maya/MFnNumericAttribute.h>
+#include <maya/MFnTypedAttribute.h>
+#include <maya/MFnStringData.h>
+
+#include <maya/MPxNode.h>
+#include <maya/MObject.h>
+#include <maya/MPlug.h>
+#include <maya/MDataBlock.h>
+#include <maya/MFnMeshData.h>
+#include <maya/MPointArray.h>
+#include <maya/MSyntax.h>
